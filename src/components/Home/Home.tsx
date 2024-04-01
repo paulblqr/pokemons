37f574bc0327bcf214e37b800c3b8715f2cae44f
paulblqr <paul@mkdx.io>
2024-04-01 12:40:32 +0200

    add a better homepage

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import style from "./Home.module.scss";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className={style.Container}>
+      <Link className={style.Link} to="/random">
+        Random
+      </Link>
+      <span className={style.Separator}>/</span>
+      <Link className={style.Link} to="/type/fire">
+        Type
+      </Link>
+    </div>
+  );
 }
